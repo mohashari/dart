99e5eabbdc2b93552ef811a2a82a9c72c7a0df5b
MOH ASHARI MUKLIS <muhashari64@gmail.com>
2019-02-10 15:38:12 +0700

    Initial Commit

---
+++ introduction.dart
@@ -0,0 +1,12 @@
+/**
+ * ini adalah pelajaran dart pertama yang akan di buat untuk memperkenalkan pemprograman dart 
+ * @Author Muklis 
+ * @email muhashari64@gmail.com
+ * Software enggine in monggopesen.com and trainer in cari guru
+ */
+
+
+
+void main(){
+  print('hello world');
+}
