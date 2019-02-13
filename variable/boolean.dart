faa558f6dd88d6c1a027d88bfa94122b24599325
MOH ASHARI MUKLIS <muhashari64@gmail.com>
2019-02-13 08:43:13 +0700

    This's Tutorial Dart bool variable

---
+++ variable/boolean.dart
@@ -0,0 +1,11 @@
+main(List<String> args) {
+  //this is variable with data type boolean
+  bool isDefault = true;
+
+  var isNormal = false;
+  print("is Normal: $isNormal");
+  print(isDefault);
+  print(isDefault.toString());
+  print(isDefault.hashCode);
+  print(isDefault.runtimeType);
+}
