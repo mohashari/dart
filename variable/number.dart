5ae01e472f5c8e3e0b511d89611ab8eea9986ee8
MOH ASHARI MUKLIS <muhashari64@gmail.com>
2019-02-19 22:07:28 +0700

    Add Num Variable

---
+++ variable/number.dart
@@ -0,0 +1,21 @@
+void main(List<String> args) {
+  int angka = 1;
+  int angka2 = 2;
+  double pecahan = 0.34;
+  double pecahan2 = 2.34;
+  num bulat = 34;
+  num phi = 3.14;
+
+  int penjumlahanAngka = angka + angka2;
+  double penjumlahanPecahan = pecahan + pecahan2;
+
+  num menghitung = phi * angka2;
+
+  print(angka);
+  print(pecahan);
+  print(bulat);
+  print(phi);
+  print(penjumlahanAngka);
+  print(penjumlahanPecahan);
+  print(menghitung);
+}
